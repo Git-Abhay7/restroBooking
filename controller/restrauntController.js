33cc97aa6715d08706dcb054c7f2997247865262
Git-Abhay7 <abhayrajawat102@gmail.com>
2020-11-12 16:10:44 +0530

    123

--- controller/restrauntController.js
+++ controller/restrauntController.js
@@ -1,32 +1,37 @@
+const moment = require("moment")
 var restraunt = require("../model/restrauntModel");
 const bcrypt = require("bcrypt");
 
 module.exports = {
     custReservation: async (req, res) => {
         try {
-            let result = await restraunt.CustomerRes(req.body.BookingDate, req.body.start, req.body.end, req.body.totalGuest,res)
-            if (result == true) {
-                let finalData = await new restraunt(req.body).save();
-                delete finalData.openTime ,finalData.closeTime;
-                res
-                    .send({
-                        responseCode: 200,
-                        resonseMessage: "Your Table Booked Successfully.", finalData
-                    });
+            let startTime = moment(req.body.openTime, 'HH:mm');
+            let endTime = moment(req.body.closeTime, 'HH:mm');
+            let timeSlots = [];
+            while (startTime <= endTime) {
+                timeSlots.push(new moment(startTime).format('HH:mm'));
+                startTime.add(15, 'minutes');
             }
-            // const moment = require("moment")
-            // let startTime = moment(req.body.openTime, 'HH:mm');
-            // let endTime = moment(req.body.closeTime, 'HH:mm');
-            // let timeSlots = [];
-            // while(startTime <= endTime){
-            //     timeSlots.push(new moment(startTime).format('HH:mm'));
-            //     startTime.add(15, 'minutes');
+            // if (req.body.totalGuest < 30) {
+                let result = await restraunt.CustomerRes(req.body.BookingDate, req.body.start, req.body.end, req.body.totalGuest, timeSlots, res, req.body.duration)
+                if (result == true) {
+                    let finalData = await new restraunt(req.body).save();
+                    delete finalData.openTime, finalData.closeTime;
+                    res
+                        .send({
+                            responseCode: 200,
+                            resonseMessage: "Your Table Booked Successfully.", finalData
+                        });
+                }
+
+            // } else {
+            //     res
+            //         .send({
+            //             responseCode: 400,
+            //             resonseMessage: "No Of Guests are more than reservation limit, kindly call restraunt for this Booking."
+            //         });
             // }
-            // console.log('timeStops ', timeSlots)
-            // res
-            //     .send({
-            //         responseMessage: "time_slot", timeSlots
-            //     });
+
         } catch (error) {
             throw error
         }
--- model/restrauntModel.js
+++ model/restrauntModel.js
@@ -1,3 +1,4 @@
+const moment = require("moment");
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 const restrauntKey = new schema(
@@ -22,6 +23,9 @@ const restrauntKey = new schema(
     },
     totalGuest: {
       type: Number
+    },
+    duration: {
+      type: Number
     }
   },
   {
@@ -33,33 +37,80 @@ const restrauntModel = mongoose.model("restraunt", restrauntKey, "restraunt");
 
 
 
-restrauntModel['CustomerRes'] = async (date, starting, ending, Guest,res) => {
+restrauntModel['CustomerRes'] = async (date, starting, ending, Guest, slots, res, duration) => {
   try {
     let custArr = [];
-    let peopleArr = []
+    let peopleArr = [];
+    let finalArray = [];
     // let customer = await restrauntModel.find({ $and: [{ BookingDate: date }, { $or: [{ end: { $gt: starting } }, { start: { $gte: starting } }] }] })
-    let customer = await restrauntModel.find({ BookingDate: date, end: { $gt: starting } })
-    if (customer.length) {
-      for (let data of customer) {
-        custArr.push(data._id)
+    // let customer = await restrauntModel.find({ BookingDate: date, end: { $gt: starting } })
+    console.log("HERE===============>>>>>>>>>>>>>>>>>>>>>>>>", date, starting, ending, Guest, slots, duration)
+
+    let customer = await restrauntModel.find({ $and: [{ BookingDate: date }, { $or: [{ end: { $gt: starting } }, { start: { $gte: starting, $lte: ending } }] }] })
+    for (let data of customer) {
+      custArr.push(data._id)
+    }
+    console.log("CUST ARRAY-====================>>>>", custArr)
+    // let users = await restrauntModel.find({ _id: { $in: custArr }, start: { $gte: starting } , end : { $lte : ending}})
+    let users = await restrauntModel.find({ _id: { $in: custArr } })
+    if (users.length) {
+      console.log("HERE======================================>>>>>>>>>>>>>>>>>>>>>>>>")
+      for (let people of users) {
+        peopleArr.push(people.totalGuest)
       }
-      // let users = await restrauntModel.find({ _id: { $in: custArr }, start: { $gte: starting } , end : { $lte : ending}})
-      let users = await restrauntModel.find({ _id: { $in: custArr } })
-      if (users.length) {
-        for (let people of users) {
-          peopleArr.push(people.totalGuest)
+      let totalPeople = peopleArr.reduce((a, b) => a + b, 0)
+      console.log("============>>>",totalPeople)
+      let newdata = Guest + totalPeople;
+      if (newdata <= 100) {
+        console.log("IDHARRRRRRRRRRRRRRRRRRRRRRRRRRRRr")
+        return true
+      } else {
+        // could be a function
+        let filterSlot = await slots.filter(k => { return k > starting })
+        console.log("filter shot =====================>>>",filterSlot)
+        for (let a of filterSlot) {
+          let spl =  a.split(":");
+          let A = parseInt(spl[0]) + duration;
+          console.log(duration,"======AAAAAAAAAAAAAAAAA===============>>>>AAAAAAAAAAAAA------------",A)
+
+          // console.log(duration,"=======AAAAAAAAAAA================>>>>a------------",a,"============",A.add(1, 'hours').format('HH:mm'),"========*********=",A)
+          let b = A.toString()+ ":" +spl[1]
+          console.log(duration,"======BBBBBBBBBBB===============>>>>b------------",a,"====",b)
+
+          if (b > a) {
+            let cust = await restrauntModel.find({ end: { $gt: a } }, { start: { $gte: a, $lte: b } })
+            let newTotalPeople = cust.totalGuest.reduce((a, b) => a + b, 0)
+            let brandNewData = newTotalPeople + Guest
+            if (brandNewData <= 100) {
+              let obj = {}
+              obj.from = a;
+              obj.to = b;
+              finalArray.push(obj)
+            }
+          }
+          // let newSlot = await restrauntModel.find({ end: { $gt: starting, lte: "23:00" } })
+          // if (newSlot.length) {
+          //   for (let k of newSlot) {
+          //     if (k.totalPeople <= Guest) {
+
+          //     }
+          //   }
+
+          //   // res.send({
+          //   //   responseCode: 400,
+          //   //   resonseMessage: "you can select booking in these slots"
+          //   // });
+          // }
+          // else {
+          //   console.log("soryy we dont have slots")
+
+          // }
         }
-        let totalPeople = peopleArr.reduce((a, b) => a + b, 0)
-        let newdata = Guest + totalPeople
-        if (totalPeople < 100 && newdata <= 100) {
-          console.log("IDHARRRRRRRRRRRRRRRRRRRRRRRRRRRRr")
-          return true
-        } else {
-          res.send({
-            responseCode: 400,
-            resonseMessage: "you can select booking in these slots"
-          });
-        }
+        res.send({
+          responseCode: 400,
+          resonseMessage: "you can select booking in these slots", finalArray
+        });
+        // return finalArray;
       }
     } else {
       return true
